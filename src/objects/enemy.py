a084515ad0981ea0973bf91dabf6f62415cbac67
PrincessSpike <epic.rabbit@mail.ru>
2019-11-21 03:35:16 +0300

    Walking of enemy is done

--- src/objects/enemy.py
+++ src/objects/enemy.py
@@ -8,10 +8,15 @@ class Enemy(Entity):
 
     def __init__(self, field, pos: Point = (0, 0), size: tuple = (1, 1)):
         super().__init__ (field, pos, size)
+        self.speed_vector = self.new_target_direction()
+
+        self.dist_from_center = 0
+        self.groving = 1
+        self.changed_block_counter = 0
 
     def new_target_direction(self):
-        d_x = {-1, 0, 1, 0}
-        d_y = {0, -1, 0, 1}
+        d_x = -1, 0, 1, 0
+        d_y = 0, -1, 0, 1
         possible_directions = []
         target_direction = Vector()
         tile_x, tile_y = self.tile
@@ -23,14 +28,30 @@ class Enemy(Entity):
             return target_direction
 
         direction_number = random.randint(1, len(possible_directions)) - 1
-        target_direction.x = possible_directions[direction_number].x
-        target_direction.y = possible_directions[direction_number].y
+        target_direction = possible_directions[direction_number]
         return target_direction
 
+    def wall_colision(self, normalized_speed_vector, past_tile):
+        self.dist_from_center += normalized_speed_vector.length * self.groving
+        print (self.dist_from_center)
+        if (self.dist_from_center > -0.000001) and (self.dist_from_center < 0.000001):
+            self.changed_block_counter += 1
+            if self.changed_block_counter == 2:
+                self.changed_block_counter = 0
+                self.speed_vector = self.new_target_direction ()
+        if (self.tile != past_tile):
+            self.dist_from_center -= normalized_speed_vector.length * self.groving
+            self.groving *= -1
 
     def process_logic(self):
-        normalized_speed_vector = self.speed_vector.normalize() * self.speed_value
+        past_tile = self.tile
+
+        normalized_speed_vector = self.speed_vector.normalized * self.speed_value
         self.pos = self.pos + normalized_speed_vector
+
+        self.wall_colision (normalized_speed_vector, past_tile)
+
+
 
 
 
--- src/scenes/game_scene.py
+++ src/scenes/game_scene.py
@@ -1,8 +1,12 @@
 from src.scenes.base_scene import Scene
 from src.objects.field import Field
+from src.objects.enemy import Enemy
 
 
 class GameScene(Scene):
     def create_objects(self):
         f = Field(self.game, (0, 0), (21, 15), (40, 40))
         self.objects.append(f)
+
+        e = Enemy(f, (1, 1))
+        self.objects.append(e)
