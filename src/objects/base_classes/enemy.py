a5ae2c4dde5a2445ee7865dc558678c92da3da3d
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2019-12-04 18:03:50 +0300

    Class Enemy fixed. Ballom works properly. Pass chases player

--- src/objects/base_classes/enemy.py
+++ src/objects/base_classes/enemy.py
@@ -4,19 +4,20 @@ from src.objects.base_classes.entity import Entity
 from src.objects.player import Player
 from src.utils.constants import Color
 from src.utils.intersections import is_circles_intersect
-from src.utils.vector import Point
+from src.utils.vector import Point, Vector
 
 
 class Enemy(Entity):
     """ Класс мобов """
     SPEED_VALUE = .5  # Скорость моба
     SCORE = 100  # Очки, выпадаемые с моба
+    delta = [Vector(1, 0), Vector(0, 1), Vector(-1, 0), Vector(0, -1)]
 
     SPRITE_CATEGORY = "enemy_sprites"
     SPRITE_DELAY = 500
-
     COLOR = Color.MAGENTA
 
+    '''
     TURN_CHANCE_MODIFIER = 1 - .05   # Больше = Реже
 
     def __init__(self, field, pos: Point = (0, 0), size: tuple = (1, 1)):
@@ -131,6 +132,80 @@ class Enemy(Entity):
         self.check_moving()  # Движение и проверка на попадание в новую клетку
 
         self.check_collisions()  # Проверка на коллизии с игроками
+
+    def hurt(self, from_):
+        """ Метод смерти моба """
+        from_.bomb_object.player_object.score += self.SCORE  # Добавляем счёт тому, чья бомба взорвала этого моба
+        self.destroy()
+    '''
+
+    def __init__(self, field, pos: Point = (0, 0), size: tuple = (1, 1)):
+        super().__init__(field, pos, size)
+
+        self.target = None  # Цель моба - соседняя клетка, в которую он переходит
+        self.direction = None  # Направление движения моба
+
+        self.animation = self.create_animation()
+
+    def can_walk_at(self, tile):
+        """ Может ли моб ходить по клетке tile? """
+        return self.field_object.tile_at(tile).walkable
+
+    def dot_product(self, vector1, vector2):
+        """ Скалярное произведение векторов """
+        return vector1.x * vector2.x + vector1.y * vector2.y
+
+    def get_new_target(self):
+        """ Обновление цели моба """
+        # Если можем продолжить движение, не меняем направление
+        if self.direction and self.can_walk_at(self.tile + self.direction):
+            return self.tile + self.direction
+        # Выбор одного из доступных направлений
+        possible_targets = []
+        for v in self.delta:
+            if self.can_walk_at(self.tile + v):
+                possible_targets.append(self.tile + v)
+        if not possible_targets:
+            return None
+        return choice(possible_targets)
+
+    def update_target_and_direction(self):
+        """ Обновление цели и направления (вызов получения цели и вычисление направления) """
+        self.target = self.get_new_target()
+        self.direction = self.target - self.tile if self.target else None
+
+    def move(self):
+        """ Движение моба """
+        # Если нет цели, пробуем ее найти
+        if not self.target:
+            self.update_target_and_direction()
+        # Если цель есть
+        if self.target:
+            # Перемещение
+            speed_vector = self.direction.normalized * self.real_speed_value
+            self.pos += speed_vector
+            # Достигнута ли цель?
+            if self.dot_product(speed_vector, self.target - self.pos) <= 0:
+                self.pos = self.target
+                self.update_target_and_direction()
+
+    def check_collisions(self):
+        """ Проверка на коллизии """
+        # Коллизия с игроком
+        for e in self.field_object.get_entities(Player):
+            if e.is_enabled:
+                r = (self.height + self.width) / 4 * self.COLLISION_MODIFIER
+                r2 = (e.height + e.width) / 4 * e.COLLISION_MODIFIER
+                if is_circles_intersect(self.pos, r, e.pos, r2):
+                    e.hurt(self)
+        # Коллизия с возникшей на пути преградой
+        if self.target and not self.can_walk_at(self.target):
+            self.direction = -self.direction
+            self.target += self.direction
+
+    def additional_logic(self):
+        self.move()
+        self.check_collisions()
 
     def hurt(self, from_):
         """ Метод смерти моба """
--- src/objects/enemies.py
+++ src/objects/enemies.py
@@ -7,11 +7,15 @@ from queue import Queue
 
 """
     Внимание! При создании логики в мобах переопределять следующие методы:
-1)_get_new_target(self) возвращает клетку цели, которую преследует моб (это игрок, или
-  конечная точка некоторой траектории, или просто соседняя клетка, если моб не слишком умный);
-2)_get_new_subtarget(self) возвращает следующую клетку на пути моба к цели;
-3)_can_walk_at(self, pos) возвращает, может ли моб ходить по клетке, заданной позицией (по
-  умолчанию метод не разрешает ходить по стенам).
+1)can_walk_at(self, pos) возвращает, может ли моб ходить по клетке, заданной позицией (по
+  умолчанию метод не разрешает ходить по стенам);
+2)get_new_target(self) возвращает цель моба - соседнюю мобом клетку, в которую он должен
+  перейти.
+  
+  Внимание! При создании логики в мобах использовать значения из следующих полей класса:
+1)self.target - текущая цель моба (None, если моб стоит на месте);
+2)self.direction - направление движения моба (единичный вектор; None, если моб стоит на месте).
+Информация в этих полях всегда корректна при вызове can_walk_at и get_new_target.
 """
 
 class Ballom (Enemy):
@@ -91,45 +95,33 @@ class Pass(Minvo):
     Если путь не единственный, то Pass старается обойти бомбу, а убегать в обратную сторону начинает
     лишь если бомбу заспавнили в 4 или меньше блоках от него на его пути к персонажу
     """
-    SPEED_VALUE = 0.5 # 2.75
+    SPEED_VALUE = 2.75
     SCORE = 4000
     COLOR = Color.APRICOT
-    delta = [Vector(1, 0), Vector(0, 1), Vector(-1, 0), Vector(0, -1)]
 
-    def _get_next_tile(self):
+    def get_new_target(self):
+        player_pos = self.field_object.get_entities(Player)[0].tile
         queue = Queue()
         parent = []
         for i in range(self.field_object.width):
             parent.append(list())
             for j in range(self.field_object.height):
-                parent[i].append(Point(-1, -1))
+                parent[i].append(None)
         parent[self.tile.x][self.tile.y] = self.tile
         queue.put(self.tile)
         while not queue.empty():
             current_pos = queue.get()
             for v in self.delta:
                 new_pos = current_pos + v
-                if self.can_walk_at(current_pos + v) and parent[new_pos.x][new_pos.y] == Point(-1, -1):
+                if self.can_walk_at(current_pos + v) and not parent[new_pos.x][new_pos.y]:
                     parent[new_pos.x][new_pos.y] = current_pos
                     queue.put(new_pos)
-        if parent[self.target.x][self.target.y] == Point(-1, -1):
-            return Point(-1, -1)
-        current_pos = self.target
+        if not parent[player_pos.x][player_pos.y]:
+            return None
+        current_pos = player_pos
         while parent[current_pos.x][current_pos.y] != self.tile:
             current_pos = parent[current_pos.x][current_pos.y]
         return current_pos
-
-    def _get_new_target(self):
-        player_object = self.field_object.get_entities(Player)[0]
-        return player_object.tile
-
-    def _get_new_subtarget(self):
-        next_pos = self._get_next_tile()
-        # print(self.target, next_pos)
-        if next_pos == Point(-1, -1):
-            return self.tile
-        else:
-            return next_pos
 
 
 class Pontan(Doria):
