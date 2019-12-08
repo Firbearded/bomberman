15423caf8bfb1f6fe99b76a93388d042891ed002
Daniil Konyagin <devdakone22@gmail.com>
2019-12-08 17:18:53 +0300

    Some optimizing in tracker

--- src/objects/field/tracker.py
+++ src/objects/field/tracker.py
@@ -1,6 +1,7 @@
-from src.utils.vector import Vector
-from src.objects.player import Player
 from queue import Queue
+
+from src.utils.vector import Vector
+
 
 class Tracker:
     """
@@ -11,10 +12,13 @@ class Tracker:
 
     def __init__(self, field_object):
         self.field_object = field_object
+        self.player = None
 
         # Матрицы путей
         self.parent = []
         self.parent_wallpass = []
+
+        self._player_last_pos = None
 
     def can_walk_at(self, tile):
         """ Функция разрешенности клетки для обычного моба """
@@ -39,7 +43,8 @@ class Tracker:
             for j in range(self.field_object.height):
                 parent[i][j] = None
         # Расчет путей до игрока обходом в ширину
-        player_pos = self.field_object.get_entities(Player)[0].tile
+        player_pos = self.player.tile
+        self._player_last_pos = player_pos
         queue = Queue()
         if walkable(player_pos):
             parent[player_pos.x][player_pos.y] = player_pos
@@ -60,12 +65,18 @@ class Tracker:
         """ Получение следующей клетки на пути для моба, который ходит через стены """
         return self.parent_wallpass[tile.x][tile.y]
 
+    def _update_player(self):
+        self.player = self.field_object.main_player
+
     def process_logic(self):
         """ Логика работы Tracker'а - расчет путей """
         # Инициализация матриц, если их размеры не соответствуют размеру поля
         if self.field_object.width != len(self.parent) or self.field_object.height != len(self.parent[0]):
             self.init_matrix(self.parent)
             self.init_matrix(self.parent_wallpass)
+        if not self.player:
+            self._update_player()
         # Cобственно расчет
-        self.calculate_ways(self.parent, self.can_walk_at)
-        self.calculate_ways(self.parent_wallpass, self.can_walk_at_wallpass)
+        if not self._player_last_pos or self._player_last_pos != self.player.tile:
+            self.calculate_ways(self.parent, self.can_walk_at)
+            self.calculate_ways(self.parent_wallpass, self.can_walk_at_wallpass)
