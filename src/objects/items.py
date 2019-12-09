ee52dc4d7bf3694d4994e01b570d9a23cea49fa4
Alexander Kildishev <king3ludey@gmail.com>
2019-12-09 03:24:26 +0300

    Mobs are being spawned after you explode the door now

--- src/objects/items.py
+++ src/objects/items.py
@@ -2,6 +2,8 @@ from src.objects.base_classes.enemy import Enemy
 from src.objects.base_classes.item import Item
 from src.objects.player import Player
 from src.utils.constants import Sounds
+from src.utils.vector import Point
+from src.objects.base_classes.base_objects.timer_object import TimerObject
 
 
 # TODO:  + Bomb-Walk + Wall-Walker + Mystery + Flame-Proof +
@@ -56,6 +58,11 @@ class MysteryUp(Item):
     def on_take(self, player_object): player_object.get_mystery()
 
 class Door(Item):
+    def __init__(self, field_object, pos: Point, size: tuple = None):
+        super().__init__(field_object, pos, size)
+        self.is_exploded = False
+
+
     SPRITE_NAMES = ("door",)
     SOUND_WIN = Sounds.Music.round_win.value
     SIZE = 1, 1
@@ -64,8 +71,16 @@ class Door(Item):
         self.game_object.mixer.channels['effects'].sound_play(self.SOUND_WIN)
         self.field_object.round_win()
 
+    def func(self): #функция, выполняющаяся при подрыве двери
+        self.is_exploded = False
+        self.field_object._generate_enemies (self.field_object._enemies_on_door, self.pos)
+
     def hurt(self, from_e):
-        pass  # self.field_object.generate_enemies(self.field_object._enemies_on_door, self.pos)  # TODO: new enemies
+        if (not self.is_exploded):
+            self.is_exploded = True
+            spawn_timer = TimerObject(505)          # Создание таймера,
+            spawn_timer.on_timeout = self.func      # призывающего мобов
+            self.game_object.add_timer(spawn_timer) # при подрыве двери
 
     def additional_logic(self):
         for e in self.field_object.get_entities(Player):
